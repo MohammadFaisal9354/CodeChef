1d01ddb0fc576863502001c94b30fda44f3469c6
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-03-25 19:23:24 +0530

    25-march-2023

---
+++ 92966093.java
@@ -0,0 +1,26 @@
+//https://www.codechef.com/problems/REACH_HOME
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int x=kk.nextInt()*5;
+		    int y=kk.nextInt();
+		    if(x>=y)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		    
+		}
+	}
+}
