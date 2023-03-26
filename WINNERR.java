4a0fd24b2ef02a913c398e47f663f69122e91d8e
MohammadFaisal9354 <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-03-26 19:11:13 +0530

    Create WINNERR.java

---
+++ WINNERR.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    int n=Math.max(kk.nextInt(),kk.nextInt());
+		    int m=Math.max(kk.nextInt(),kk.nextInt());
+		    if(n<m)
+		    {
+		        System.out.println("P");
+		    }
+		    else if(m<n)
+		    System.out.println("Q");
+		    else
+		    System.out.println("TIE");
+		}
+	}
+}
