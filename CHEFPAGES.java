c0e447d2362aad5b2c4f5b92148df2bfec2f6ec8
Mohammad Faisal <faisalkhan9354@gmail.com>
2023-05-07 06:27:38 +0530

    7 may 2023

---
+++ CHEFPAGES.java
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/CHEFPAGES
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		    int a=kk.nextInt();
+		    int b=kk.nextInt();
+		    if(a==1&&b==1)
+		    System.out.println("https://discuss.codechef.com");
+		    else if(a==1)
+		    System.out.println("https://www.codechef.com/contests");
+		    else
+		    System.out.println("https://www.codechef.com/practice");
+	}
+}
