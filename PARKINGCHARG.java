01e1c1d9acd00861ce7bea74acfd74e00368fc3e
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-05-26 23:30:40 +0530

    Create PARKINGCHARG.java

---
+++ PARKINGCHARG.java
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/PARKINGCHARG
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		{
+		    int x=kk.nextInt();
+		    int y=kk.nextInt();
+		    int h=kk.nextInt();
+		    System.out.println(x+((h-1)*y));		    
+		}
+	}
+}
