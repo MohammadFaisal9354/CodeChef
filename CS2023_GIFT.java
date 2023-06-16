80a6edaed353ec60caf4f770761c92588ec0cc88
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-06-16 07:38:37 +0530

    Create CS2023_GIFT.java

---
+++ CS2023_GIFT.java
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/CS2023_GIFT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		    int moneyHave=kk.nextInt();
+		    int laptop=kk.nextInt();
+		    int fund=kk.nextInt();
+		    System.out.println((moneyHave+fund>=laptop)?"YES":"NO");
+  }
+}
