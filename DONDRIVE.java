6ea6dc4b17fc602d6f07146bf31000b9d91f5357
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-06-22 14:51:20 +0530

    Create DONDRIVE.java

---
+++ DONDRIVE.java
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/DONDRIVE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		int testcases=kk.nextInt();
+		while(testcases-->0)
+		{
+		    System.out.println(kk.nextInt()-kk.nextInt());
+		    
+		}
+	}
+}
