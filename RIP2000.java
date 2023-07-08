f82cbe14e41812413190034e3b5e3f199e8f2d09
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-07-08 10:12:45 +0530

    Create RIP2000.java

---
+++ RIP2000.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+	    System.out.println(kk.nextInt()*4);
+	}
+}
