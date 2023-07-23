4e8f4671f2fe9903100c83e93501e9305b6a1aba
Mohammad Faisal <114982586+MohammadFaisal9354@users.noreply.github.com>
2023-07-23 10:59:24 +0530

    Create PUZHUNT.java

---
+++ PUZHUNT.java
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/PUZHUNT
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Faisal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner kk=new Scanner(System.in);
+		    int n=kk.nextInt();
+		    if(n>5&&n<9)
+		    System.out.println("Yes");
+		    else
+		    System.out.println("No");
+	}
+}
